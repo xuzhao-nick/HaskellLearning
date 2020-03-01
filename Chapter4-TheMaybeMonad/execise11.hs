bf163693c6a753708b2523b2dc2c99ac92c16299
Nick Xu <brainstudiosg@gmail.com>
2020-03-01 17:36:27 +0800

    Update execise11.hs

--- Chapter4-TheMaybeMonad/execise11.hs
+++ Chapter4-TheMaybeMonad/execise11.hs
@@ -3,3 +3,6 @@ bindMaybe maybeValue aFunction =
   case maybeValue of
       Nothing -> Nothing
       Just value -> aFunction value
+
+bindMaybe1 Nothing aFunction = Nothing
+bindMaybe1 (Just x) aFunction = aFunction x
