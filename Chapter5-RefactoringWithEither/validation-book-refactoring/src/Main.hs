ce8d923f1902a1471b477c164a09daa0bd3b5d9c
Nick Xu <brainstudiosg@gmail.com>
2020-03-01 19:04:20 +0800

    Update Main.hs

--- Chapter5-RefactoringWithEither/validation-book-refactoring/src/Main.hs
+++ Chapter5-RefactoringWithEither/validation-book-refactoring/src/Main.hs
@@ -1,5 +1,55 @@
 module Main where
+import Data.Char
+
+cleanWhitespace :: String -> Maybe String
+cleanWhitespace "" = Nothing
+cleanWhitespace (x : xs) =
+    case (isSpace x) of
+        True -> cleanWhitespace xs
+        False -> Just (x : xs)
+
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum xs =
+    case (all isAlphaNum xs) of
+        False -> Nothing
+        True -> Just xs
+
+checkPasswordLength :: String -> Maybe String
+checkPasswordLength password =
+    case (length password > 20)  of
+        True -> Nothing
+        False -> Just password
+
+checkPassword :: String -> Maybe String
+checkPassword password =
+    case (cleanWhitespace password) of
+        Nothing -> Nothing
+        Just cleanedPassword ->
+            case (checkPasswordLength cleanedPassword) of
+                Nothing -> Nothing
+                Just cleanedPassword ->
+                    case (requireAlphaNum cleanedPassword) of
+                        Nothing -> Nothing
+                        Just cleanedPassword -> Just cleanedPassword
+
+validatePassword :: String -> String
+validatePassword password =
+    case (cleanWhitespace password) of
+        Nothing -> "Your password cannot be empty."
+        Just password2 ->
+            case (requireAlphaNum password2) of
+                Nothing -> "Your password cannot contain \
+                           \white space or special characters."
+                Just password3 ->
+                    case (checkPasswordLength password3) of
+                        Nothing -> "Your password cannot be \
+                                   \longer than 20 characters."
+                        Just password4 -> password4
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main =
+  do
+    putStrLn "Please enter a password\n>"
+    password <- getLine
+    print(validatePassword password)
