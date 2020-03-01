5702af1b52fe1c89cbaa3d7e60434fd82523d1af
Nick Xu <brainstudiosg@gmail.com>
2020-03-01 17:53:44 +0800

    Update execise11.hs

--- Chapter4-TheMaybeMonad/execise11.hs
+++ Chapter4-TheMaybeMonad/execise11.hs
@@ -1,3 +1,19 @@
+import Data.Char
+cleanWhitespace :: String -> Maybe String
+cleanWhitespace "" = Nothing
+cleanWhitespace (x : xs) =
+    case (isSpace x) of
+        True -> cleanWhitespace xs
+        False -> Just (x : xs)
+
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum xs =
+    case (all isAlphaNum xs) of
+        False -> Nothing
+        True -> Just xs
+
+
 bindMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
 bindMaybe maybeValue aFunction =
   case maybeValue of
@@ -6,3 +22,8 @@ bindMaybe maybeValue aFunction =
 
 bindMaybe1 Nothing aFunction = Nothing
 bindMaybe1 (Just x) aFunction = aFunction x
+-- Usage Examples
+-- bindMaybe (cleanWhitespace "  abc") requireAlphaNum
+-- bindMaybe1 (cleanWhitespace "  abc") requireAlphaNum
+-- (cleanWhitespace "  abc") `bindMaybe` requireAlphaNum
+-- (cleanWhitespace "  abc") `bindMaybe1` requireAlphaNum
