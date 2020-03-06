03dde3ab4ec8683b64f2951c46b469564c45eaac
Nick Xu <brainstudiosg@gmail.com>
2020-03-06 20:22:26 +0800

    Add unit test

--- Chapter5-RefactoringWithEither/validation-book-refactoring/src/Main.hs
+++ Chapter5-RefactoringWithEither/validation-book-refactoring/src/Main.hs
@@ -29,9 +29,37 @@ validatePassword password =
         >>= requireAlphaNum
         >>= checkPasswordLength
 
+
 main :: IO ()
 main =
   do
     putStrLn "Please enter a password\n>"
     password <- getLine
     print(validatePassword password)
+
+printTestResult :: Either String () -> IO()
+printTestResult r =
+    case r of
+        Left err -> putStrLn err
+        Right () -> putStrLn "All tests passed."
+
+eq :: (Eq a, Show a) => Int -> a -> a -> Either String ()
+eq n actual expected =
+    case (actual == expected) of
+        True -> Right ()
+        False -> Left (unlines
+          [ "Test" ++ show n
+          , " Expected: " ++ show expected
+          , " But got:  " ++ show actual
+          ])
+
+test :: IO ()
+test = printTestResult $
+  do
+    eq 1 (checkPasswordLength "") (Right "")
+    eq 2 (checkPasswordLength "julielovesbooks")
+         (Right "julielovesbooks")
+    eq 3 (requireAlphaNum "julielovesbooks")
+              (Right "julielovesbooks")
+    eq 4 (cleanWhitespace "  julie")
+         (Right "julie")
